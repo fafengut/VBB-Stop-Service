d2f687a1fc0298ea0f999ee5617585b8b1c65f6a
Paul Fafengut <paul.fafengut93@gmail.com>
2022-09-20 15:42:04 +0200

    added view-router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup>
-</script>
-
 <template>
+  <nav>
+    <router-link :to="{name: 'home'}">Home</router-link> |
+    <router-link :to="{name: 'about'}">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<style scoped>
+<script>
+</script>
+
+<style>
 </style>
