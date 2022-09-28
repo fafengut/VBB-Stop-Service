52ae0aa174f3e596a6c32dc4b7bc106b0ef30c1e
Paul Fafengut <paul.fafengut93@gmail.com>
2022-09-28 20:02:14 +0200

    deployed by gh-deploy.sh

---
+++ assets/HomeView.61682292.js
@@ -0,0 +1,1 @@
+import c from"./StopSuche.83e04626.js";import{o as e,c as o,a as r,F as _,b as n,d as m}from"./index.81b87fab.js";import"./favorite.ff9442b4.js";const l=n("h1",{class:"text-6xl"},"Haltestelle suchen",-1),p={__name:"StopView",setup(t){return(a,s)=>(e(),o(_,null,[l,r(c)],64))}},d={__name:"HomeView",setup(t){return(a,s)=>(e(),m(p))}};export{d as default};
---
+++ assets/stopsDetails.2b8e1a02.js
@@ -0,0 +1,1 @@
+import{a}from"./favorite.ff9442b4.js";const r="stops",i={findById:async(t,s)=>await a.get(`${r}/${t}`,s),findDepartures:async(t,s)=>await a.get(`${r}/${t}/departures`,s)};export{i as s};
