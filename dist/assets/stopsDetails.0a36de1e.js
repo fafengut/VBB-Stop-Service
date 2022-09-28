d67ca53d2f351e0415ef0d9abf6376089ef550df
Paul Fafengut <65964109+fafengut@users.noreply.github.com>
2022-09-28 11:38:52 +0200

    Deploy (#9)
    * adding dist
    
    * added deploy script

---
+++ dist/assets/stopsDetails.0a36de1e.js
@@ -0,0 +1,1 @@
+import{a}from"./favorite.353f34c3.js";const r="stops",i={findById:async(t,s)=>await a.get(`${r}/${t}`,s),findDepartures:async(t,s)=>await a.get(`${r}/${t}/departures`,s)};export{i as s};
